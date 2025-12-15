7a4564087fbdcf4de3aaab7a10bf4c9852eda834
Axel Pincheira <teck007@gmail.com>
2025-12-15 18:50:46 -0300

    add @astrojs/node adapter to astro.config.mjs for standalone server deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 export default defineConfig({
   output: 'server',
   integrations: [tailwind(), react()],
+  adapter: node({
+    mode: 'standalone',
+  }),
   vite: {
     resolve: {
       alias: {
